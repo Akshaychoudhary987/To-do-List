5f506a249deb5c49cd56129f9dc528e56dea23ed
akshaychaudhary <akshaychaudhary3475@gmail.com>
2025-07-17 00:09:02 -0700

    Initial commit for To-do List project

--- index.js
+++ index.js
@@ -10,32 +10,31 @@ button.addEventListener('click', () => {
     return;
   }
 
-  // Create a div to hold the task
+
   const resultdiv = document.createElement('div');
   resultdiv.className = "resultdiv";
 
-  // Create the task text
+ 
   const h4 = document.createElement('h4');
   h4.innerText = inputvalue;
 
-  // Create delete button
+  
   const delbtn = document.createElement('button');
   delbtn.innerText = "❌";
   delbtn.className = 'delete-btn';
 
-  // Add event to delete the task
+  
   delbtn.addEventListener('click', () => {
     resultdiv.remove();
   });
 
-  // Append h4 and button to the div
+  
   resultdiv.appendChild(h4);
   resultdiv.appendChild(delbtn);
 
-  // Add the task to the todo-app container
   todoapp.appendChild(resultdiv);
 
-  // Clear input
+
   input.value = "";
 });
 
